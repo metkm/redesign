bddeb3e4c2ef0c1b86f7a16ca053f4cf24dd5ab2
metkm <metinkorkmaz417@gmail.com>
2025-06-18 17:57:52 +0300

    disable devtools

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-05-15',
-  devtools: { enabled: true },
   modules: ['@nuxt/ui', '@nuxt/fonts', '@nuxt/eslint'],
+  devtools: { enabled: false },
   css: ['~/assets/css/main.css'],
+  compatibilityDate: '2025-05-15',
   eslint: {
     config: {
       stylistic: true,
-    }
-  }
+    },
+  },
 })
